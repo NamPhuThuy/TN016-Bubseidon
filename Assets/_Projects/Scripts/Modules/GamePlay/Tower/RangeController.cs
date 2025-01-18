dba0ae880fbb30ceb3e31a64ce9ac1cb1fa25461
NamPhuThuy <ttnam957@gmail.com>
2025-01-18 13:15:47 +0700

    delete: usage of _curentTarget in RangeController.cs

--- Assets/_Projects/Scripts/Modules/GamePlay/Tower/RangeController.cs
+++ Assets/_Projects/Scripts/Modules/GamePlay/Tower/RangeController.cs
@@ -8,7 +8,6 @@ public class RangeController : MonoBehaviour
     [SerializeField] private float _attackRange = 1.5f;
     
     [SerializeField] private CircleCollider2D _circleCollider2D;
-    [SerializeField] private EnemyController _currentTarget;
 
     private Transform _transform;
     void Start()
@@ -18,31 +17,7 @@ public class RangeController : MonoBehaviour
         _transform = transform;
     }
     
-    private void Update()
-    {
-        if (_currentTarget != null)
-            return;
-        
-        _currentTarget = FindClosestEnemy();
-    }
-    
-    private EnemyController FindClosestEnemy()
-    {
-        var minDistance = float.MaxValue;
-        EnemyController target = null;
-        
-        foreach (EnemyController enemy in GamePlayManager.Instance.EnemyList)
-        {
-            float sqrMagnitude = Vector2.SqrMagnitude(_transform.position - enemy.transform.position);
-            if (sqrMagnitude < minDistance)
-            {
-                minDistance = sqrMagnitude;
-                target = enemy.gameObject.GetComponent<EnemyController>();
-            }
-        }
-
-        return target;
-    }
+   
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log($"TNam - {other.transform.name} in tower range");
@@ -60,8 +35,6 @@ public class RangeController : MonoBehaviour
         {
             case "Enemy":
                 _tower.StopAllCoroutines();
-                // StopCoroutine(Attack(_currentTarget));
-                _currentTarget = null;
                 break;
         }
     } 
