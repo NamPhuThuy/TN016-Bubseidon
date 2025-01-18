656d102c5cf90609fccf02d788bd2958d3dca9e0
kinatraa <hakien.237@gmail.com>
2025-01-19 01:02:30 +0700

    add: title screen effect

--- Assets/_Projects/Scripts/Modules/UI/UIScreen/UIScreenTitle.cs
+++ Assets/_Projects/Scripts/Modules/UI/UIScreen/UIScreenTitle.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using NamPhuThuy;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,9 +11,27 @@ public class UIScreenTitle : UIScreenBase
     [Header("Buttons")]
     [SerializeField] private Button _clickToStartButton;
     
+    [SerializeField] private TextMeshProUGUI _titleText;
+    public float flickerInterval = 0.3f; // Time between flickers
+
+    private bool isVisible = true;
+    
     public override void Show()
     {
         gameObject.SetActive(true);
+        StartCoroutine(Flicker());
+    }
+    
+    private IEnumerator Flicker()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(flickerInterval);
+
+            // Toggle visibility
+            isVisible = !isVisible;
+            _titleText.enabled = isVisible;
+        }
     }
 
     public override void Hide()
