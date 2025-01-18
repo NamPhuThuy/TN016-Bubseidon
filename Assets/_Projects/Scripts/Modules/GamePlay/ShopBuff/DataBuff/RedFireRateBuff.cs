68f82c75247bc2b08681161f4001d0940f2a2be0
kinatraa <hakien.237@gmail.com>
2025-01-19 03:27:40 +0700

    fix: build webGL

--- Assets/_Projects/Scripts/Modules/GamePlay/ShopBuff/DataBuff/RedFireRateBuff.cs
+++ Assets/_Projects/Scripts/Modules/GamePlay/ShopBuff/DataBuff/RedFireRateBuff.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
 [CreateAssetMenu(fileName = "RedFireRateBuff",menuName = "Buff/RedFireRateBuff",order = 0)]
 public class RedFireRateBuff :Buff {
--- Assets/_Projects/Scripts/Modules/GamePlay/ShopBuff/DataBuff/RedRangeBuff.cs
+++ Assets/_Projects/Scripts/Modules/GamePlay/ShopBuff/DataBuff/RedRangeBuff.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
 [CreateAssetMenu(fileName = "RedRangeBuff",menuName = "Buff/RedRangeBuff",order = 0)]
 public class RedRangeBuff:Buff {
